fce1b716900dca6b9380ca67fe447535c702c32c
irenegarzona <62965324+irenegarzona@users.noreply.github.com>
2020-03-31 15:29:52 -0600

    taller git
    .

---
+++ main.py
@@ -0,0 +1,11 @@
+""" Taller de Github 31 de marzo del 2020"""
+
+#Commit: guardar los cambios localmente en git
+#Push: subir los cambios a la nube
+#Pull: Descargar el código del github
+#Merge: unión del código local con el que está en la nube
+
+print("Hello world")
+
+if(1==1):
+    print(True)
